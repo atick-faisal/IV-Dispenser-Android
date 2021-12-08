3fe9b7d56e2fd495365cf9f05760db24cc1dec82
Atick Faisal <atickfaisal@gmail.com>
2021-12-08 22:56:07 +0600

    Add data module

--- data/src/main/kotlin/dev/atick/data/database/room/AppDatabase.kt
+++ data/src/main/kotlin/dev/atick/data/database/room/AppDatabase.kt
@@ -1,19 +1,19 @@
 package dev.atick.data.database.room
 
-//import androidx.room.Database
-//import androidx.room.RoomDatabase
-//import dev.atick.data.models.Dispenser
-//import dev.atick.data.models.DispenserState
-//
-//@Database(
-//    version = 1,
-//    exportSchema = false,
-//    entities = [
-//        Dispenser::class,
-//        DispenserState::class
-//    ]
-//)
-//abstract class AppDatabase : RoomDatabase() {
-//    abstract val dispenserDao: DispenserDao
-//    abstract val dispenserStateDao: DispenserStateDao
-//}
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import dev.atick.data.models.Dispenser
+import dev.atick.data.models.DispenserState
+
+@Database(
+    version = 1,
+    exportSchema = false,
+    entities = [
+        Dispenser::class,
+        DispenserState::class
+    ]
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract val dispenserDao: DispenserDao
+    abstract val dispenserStateDao: DispenserStateDao
+}
--- data/src/main/kotlin/dev/atick/data/database/room/DispenserStateDao.kt
+++ data/src/main/kotlin/dev/atick/data/database/room/DispenserStateDao.kt
@@ -1,32 +1,32 @@
 package dev.atick.data.database.room
 
-//import androidx.room.*
-//import dev.atick.data.models.DispenserState
-//import kotlinx.coroutines.flow.Flow
-//
-//@Dao
-//interface DispenserStateDao {
-//    @Insert(onConflict = OnConflictStrategy.REPLACE)
-//    suspend fun insert(dispenserState: DispenserState)
-//
-//    @Update
-//    suspend fun update(dispenserState: DispenserState)
-//
-//    @Delete
-//    suspend fun delete(dispenserState: DispenserState)
-//
-//    @Query("SELECT * FROM dispenser_state_table WHERE id = :id")
-//    suspend fun getItemById(id: Long): DispenserState?
-//
-//    @Query("SELECT * FROM dispenser_state_table WHERE device_id = :deviceId")
-//    fun getItemByDeviceId(deviceId: String): Flow<List<DispenserState>>
-//
-//    @Query("SELECT * FROM dispenser_state_table")
-//    fun getAllItems(): Flow<List<DispenserState>>
-//
-//    @Insert(onConflict = OnConflictStrategy.REPLACE)
-//    suspend fun insertAll(items: List<DispenserState>)
-//
-//    @Query("DELETE FROM dispenser_state_table")
-//    suspend fun clear()
-//}
+import androidx.room.*
+import dev.atick.data.models.DispenserState
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DispenserStateDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(dispenserState: DispenserState)
+
+    @Update
+    suspend fun update(dispenserState: DispenserState)
+
+    @Delete
+    suspend fun delete(dispenserState: DispenserState)
+
+    @Query("SELECT * FROM dispenser_state_table WHERE id = :id")
+    suspend fun getItemById(id: Long): DispenserState?
+
+    @Query("SELECT * FROM dispenser_state_table WHERE device_id = :deviceId")
+    fun getItemByDeviceId(deviceId: String): Flow<List<DispenserState>>
+
+    @Query("SELECT * FROM dispenser_state_table")
+    fun getAllItems(): Flow<List<DispenserState>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(items: List<DispenserState>)
+
+    @Query("DELETE FROM dispenser_state_table")
+    suspend fun clear()
+}
