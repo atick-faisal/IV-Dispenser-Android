40db60fab232915e7ac2ec1ac375c9cd6ae62f9d
Atick Faisal <atickfaisal@gmail.com>
2021-12-13 19:56:03 +0600

    Add branding to the login screen

--- app/src/main/kotlin/dev/atick/compose/ui/login/LoginScreen.kt
+++ app/src/main/kotlin/dev/atick/compose/ui/login/LoginScreen.kt
@@ -2,7 +2,9 @@ package dev.atick.compose.ui.login
 
 import android.animation.ValueAnimator
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -17,7 +19,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,76 +41,101 @@ fun LoginScreen(
     val username = remember { mutableStateOf("") }
     val password = remember { mutableStateOf("") }
 
-    return Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.surface)
-            .padding(24.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        AndroidView(
-            factory = { ctx ->
-                LottieAnimationView(ctx).apply {
-                    setAnimation(R.raw.server_alt)
-                    repeatCount = ValueAnimator.INFINITE
-                    speed = 0.5F
-                }
-            },
-            update = {
-                if (viewModel.loginProgress.value) {
-                    it.playAnimation()
-                } else {
-                    it.pauseAnimation()
-                }
-            },
+    return Box(modifier = Modifier.fillMaxSize()) {
+        Column(
             modifier = Modifier
-                .width(120.dp)
-                .height(120.dp)
-        )
-        Spacer(modifier = Modifier.height(32.dp))
-        Text(
-            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
-            text = "Please Login to the Hospital's Server",
-            color = MaterialTheme.colors.onSurface,
-            fontSize = 24.sp,
-            textAlign = TextAlign.Center
-        )
-        Spacer(modifier = Modifier.height(32.dp))
-        LoginTextFiled(
-            textFieldValue = username.value,
-            labelResourceId = R.string.label_username,
-            hintResourceId = R.string.hint_username,
-            leadingIcon = {
-                Icon(imageVector = Icons.Filled.Person, contentDescription = "")
-            },
-            onValueChange = {
-                username.value = it
-            }
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        LoginTextFiled(
-            textFieldValue = password.value,
-            isPasswordField = true,
-            labelResourceId = R.string.label_password,
-            hintResourceId = R.string.hint_password,
-            leadingIcon = {
-                Icon(imageVector = Icons.Filled.Password, contentDescription = "")
-            },
-            onValueChange = {
-                password.value = it
-            }
-        )
-        Spacer(modifier = Modifier.height(24.dp))
-        Button(
-            onClick = { viewModel.login() },
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(48.dp),
-            shape = RoundedCornerShape(16.dp)
+                .fillMaxSize()
+                .background(MaterialTheme.colors.surface)
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Text(text = stringResource(R.string.login))
+            AndroidView(
+                factory = { ctx ->
+                    LottieAnimationView(ctx).apply {
+                        setAnimation(R.raw.server_alt)
+                        repeatCount = ValueAnimator.INFINITE
+                        speed = 0.5F
+                    }
+                },
+                update = {
+                    if (viewModel.loginProgress.value) {
+                        it.playAnimation()
+                    } else {
+                        it.pauseAnimation()
+                    }
+                },
+                modifier = Modifier
+                    //.width(64.dp)
+                    .fillMaxHeight(0.12F)
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Text(
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                text = "Please Login to the Hospital's Server",
+                color = MaterialTheme.colors.onSurface,
+                fontSize = 24.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            LoginTextFiled(
+                textFieldValue = username.value,
+                labelResourceId = R.string.label_username,
+                hintResourceId = R.string.hint_username,
+                leadingIcon = {
+                    Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+                },
+                onValueChange = {
+                    username.value = it
+                }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            LoginTextFiled(
+                textFieldValue = password.value,
+                isPasswordField = true,
+                labelResourceId = R.string.label_password,
+                hintResourceId = R.string.hint_password,
+                leadingIcon = {
+                    Icon(imageVector = Icons.Filled.Password, contentDescription = "")
+                },
+                onValueChange = {
+                    password.value = it
+                }
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Button(
+                onClick = { viewModel.login() },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp),
+                shape = RoundedCornerShape(16.dp)
+            ) {
+                Text(text = stringResource(R.string.login))
+            }
+            Spacer(modifier = Modifier.fillMaxHeight(0.4F))
         }
-        Spacer(modifier = Modifier.fillMaxHeight(0.4F))
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier.width(120.dp),
+                painter = painterResource(id = R.drawable.qu_logo),
+                contentDescription = "Qatar University Logo"
+            )
+            
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "Qatar University\nMachine Learning Group",
+                color = if (isSystemInDarkTheme()) Color.LightGray else Color.DarkGray,
+                textAlign = TextAlign.Center,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Medium
+            )
+        }
     }
 }
