8362a27777fa4230336286cf08856c6056d94d1d
Atick Faisal <atickfaisal@gmail.com>
2021-12-30 21:40:46 +0600

    Fix mqtt repository issue

--- mqtt/src/main/kotlin/dev/atick/mqtt/di/RepositoryModule.kt
+++ mqtt/src/main/kotlin/dev/atick/mqtt/di/RepositoryModule.kt
@@ -6,11 +6,13 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dev.atick.mqtt.repository.MqttRepository
 import dev.atick.mqtt.repository.MqttRepositoryImpl
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
     @Binds
+    @Singleton
     abstract fun bindMqttRepository(
         mqttRepositoryImpl: MqttRepositoryImpl
     ): MqttRepository
