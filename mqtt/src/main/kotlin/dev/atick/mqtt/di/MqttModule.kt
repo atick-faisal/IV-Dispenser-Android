f187bbd147716c02cc41c25351f33e5bb939e7df
Atick Faisal <atickfaisal@gmail.com>
2022-05-12 08:41:21 +0300

    Create a global server build

--- mqtt/src/main/kotlin/dev/atick/mqtt/di/MqttModule.kt
+++ mqtt/src/main/kotlin/dev/atick/mqtt/di/MqttModule.kt
@@ -15,8 +15,8 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object MqttModule {
 
-    // const val BROKER_URL = "broker.emqx.io"
-    const val BROKER_URL = "192.168.0.110"
+    const val BROKER_URL = "broker.emqx.io"
+    // const val BROKER_URL = "192.168.0.110"
     const val BROKER_PORT = 1883
 
     @Singleton
